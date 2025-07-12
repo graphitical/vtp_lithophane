ffde8c051fa8dfdf4546a853ad9ce7c24c588297
Daniel Revier <revier.daniel@gmail.com>
2025-07-12 11:31:42 -0700

    Add in a commented out version of the (1.5,1.5) LUT in case we want to swap later

--- src/gcode/lut.py
+++ src/gcode/lut.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
+# for (1.5, 4.5) dL pair
 # TODO: Change the name because it isn't actually interpolated.
 def _build_image_lut() -> dict[int, dict[int, np.ndarray]]:
     """
@@ -32,5 +33,38 @@ def _build_image_lut() -> dict[int, dict[int, np.ndarray]]:
         }
     }
 
+    import numpy as np
+
+
+# for (1.5, 1.5) dL pair
+# def _build_image_lut() -> dict[int, dict[int, np.ndarray]]:
+#     """
+#     Builds a lookup table (LUT) for interpolating lithophane layer values into two layers.
+#     The LUT is constructed based on known values for specific quantization levels from rendered images.
+#     """
+#     return {
+#         2: {
+#             0: np.array([[255, 255, 0], [204, 153, 0]], dtype=np.uint8),
+#             1: np.array([[0, 0, 0], [0, 0, 0]], dtype=np.uint8),
+#         },
+#         3: {
+#             0: np.array([[255, 255, 0], [204, 153, 0]], dtype=np.uint8),
+#             1: np.array([[101, 255, 0], [101, 153, 0]], dtype=np.uint8),
+#             2: np.array([[0, 0, 0], [0, 0, 0]], dtype=np.uint8),
+#         },
+#         4: {
+#             0: np.array([[255, 255, 0], [204, 153, 0]], dtype=np.uint8),
+#             1: np.array([[153, 51, 0], [204, 102, 0]], dtype=np.uint8),
+#             2: np.array([[204, 153, 0], [153, 0, 0]], dtype=np.uint8),
+#             3: np.array([[0, 0, 0], [0, 0, 0]], dtype=np.uint8),
+#         },
+#         5: {
+#             0: np.array([[255, 255, 0], [204, 153, 0]], dtype=np.uint8),
+#             1: np.array([[255, 204, 0], [0, 51, 0]], dtype=np.uint8),
+#             2: np.array([[101, 255, 0], [101, 153, 0]], dtype=np.uint8),
+#             3: np.array([[101, 153, 0], [204, 153, 0]], dtype=np.uint8),
+#             4: np.array([[0, 0, 0], [0, 0, 0]], dtype=np.uint8),
+#         }
+#     }
 
 LUT = _build_image_lut()
